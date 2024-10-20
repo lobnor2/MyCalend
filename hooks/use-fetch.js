8a786943608d680f717c5aa0a45f77ebe97dee40
lobsang norbu <lnorbu151@gmail.com>
2024-10-20 02:34:40 -0400

    add description or comment for better understanding

--- hooks/use-fetch.js
+++ hooks/use-fetch.js
@@ -1,15 +1,20 @@
+// Reuse this hooks for fetching api calls, no need to write code for all the api calls
+//custom hooks are like function in js
 import { useState } from "react";
 
+//it takes a callback function
 const useFetch = (cb) => {
   const [data, setData] = useState(undefined);
   const [loading, setLoading] = useState(null);
   const [error, setError] = useState(null);
 
+  //it will take additional arguments if user is providing
   const fn = async (...args) => {
     setLoading(true);
     setError(null);
 
     try {
+      //provide arg to callback
       const response = await cb(...args);
       setData(response);
       setError(null);
