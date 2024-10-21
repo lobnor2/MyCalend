0c5d03e6acf36e099f69e236bed2839507dcc6f9
lobsang norbu <lnorbu151@gmail.com>
2024-10-21 11:05:02 -0400

    feat: add delete functionality

--- actions/events.js
+++ actions/events.js
@@ -55,3 +55,34 @@ export async function getUserEvents() {
   });
   return { events, username: user.username };
 }
+
+export async function deleteEvent(eventId) {
+  const { userId } = auth();
+
+  //check authorization
+  if (!userId) {
+    throw new Error("Unauthorized");
+  }
+
+  //check if user already exists
+  const user = await db.user.findUnique({
+    where: { clerkUserId: userId },
+  });
+  if (!user) {
+    throw new Error("User not found");
+  }
+  //fetch event
+  const event = await db.event.findUnique({
+    where: { id: eventId },
+  });
+
+  //check if event not exists or doesnt belong to that particular user
+  if (!event || event.userId !== user.id) {
+    throw new Error("Event not found or unauthorized");
+  }
+  //if everything is fine, delete event
+  await db.event.delete({
+    where: { id: eventId },
+  });
+  return { success: true };
+}
