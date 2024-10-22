3b1c2edcff3fd36cc6eaf29e67a8a1640b9da7b2
lobsang norbu <lnorbu151@gmail.com>
2024-10-22 12:41:26 -0400

    add get user availability server action

---
+++ actions/availability.js
@@ -0,0 +1,53 @@
+import { db } from "@/lib/prisma";
+import { auth } from "@clerk/nextjs/server";
+
+export async function getUserAvailability() {
+  const { userId } = auth();
+  //check user authorization
+  if (!userId) {
+    throw new Error("Unauthorized");
+  }
+
+  //check if user exits and user is available or not
+  const user = await db.user.findUnique({
+    where: { clerkUserId: userId },
+    include: {
+      availability: {
+        include: { days: true },
+      },
+    },
+  });
+  //no user or user availability
+  if (!user || !user.availability) {
+    return null;
+  }
+
+  const availabilityData = {
+    timeGap: user.availability.timeGap,
+  };
+
+  [
+    "monday",
+    "tuesday",
+    "wednesday",
+    "thursday",
+    "friday",
+    "saturday",
+    "sunday",
+  ].forEach((day) => {
+    const dayAvailability = user.availability.days.find(
+      (d) => d.day === day.toUpperCase()
+    );
+    availabilityData[day] = {
+      isAvailable: !!dayAvailability,
+      startTime: dayAvailability
+        ? dayAvailability.startTime.toISOString().slice(11, 16)
+        : "09:00",
+      endTime: dayAvailability
+        ? dayAvailability.endTime.toISOString().slice(11, 16)
+        : "17:00",
+    };
+  });
+
+  return availabilityData;
+}
