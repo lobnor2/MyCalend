a748c839802959afd50371fc9dab965b52f595e8
lobsang norbu <lnorbu151@gmail.com>
2024-10-28 18:09:21 -0400

    move constant data to new file

---
+++ app/lib/constant.js
@@ -0,0 +1,38 @@
+import { Calendar, Clock, LinkIcon } from "lucide-react";
+
+export const features = [
+  {
+    icon: Calendar,
+    title: "Create Events",
+    description: "Easily set up and customize your event types",
+  },
+  {
+    icon: Clock,
+    title: "Manage Availability",
+    description: "Define your abailability to streamline scheduling ",
+  },
+  {
+    icon: LinkIcon,
+    title: "Custom Links",
+    description: "Share your personalized scheduling link",
+  },
+];
+
+export const howItWorks = [
+  {
+    step: "Sign Up",
+    description: "Create your free mycalend account",
+  },
+  {
+    step: "Set Availability",
+    description: "Define when you're available for meetings",
+  },
+  {
+    step: "Share Your Link",
+    description: "Send your scheduling link to clients or colleagues",
+  },
+  {
+    step: "Get Booked",
+    description: "Receive confirmations for new appointments automatically",
+  },
+];
