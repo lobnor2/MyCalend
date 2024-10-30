45dbe7e8411c3751771c3e07637efb644767c2cc
lobsang norbu <lnorbu151@gmail.com>
2024-10-30 00:52:54 -0400

    change font weight for not found page

--- app/not-found.jsx
+++ app/not-found.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const NotFound = () => {
   return (
-    <div className="text-4xl font-extralight border border-blue-500 flex justify-center items-center h-screen">
+    <div className="text-4xl font-extralight flex justify-center items-center h-screen">
       404 Page NotFound
     </div>
   );
