c43cdfc39f509ae62ef6eab18e8893e8e5d0cc4b
lobsang norbu <lnorbu151@gmail.com>
2024-11-01 10:53:27 -0400

    add username field in the getEventDetails

--- actions/events.js
+++ actions/events.js
@@ -86,3 +86,26 @@ export async function deleteEvent(eventId) {
   });
   return { success: true };
 }
+
+export async function getEventDetails(username, eventId) {
+  const event = await db.event.findFirst({
+    where: {
+      id: eventId,
+      user: {
+        username: username,
+      },
+    },
+    include: {
+      user: {
+        select: {
+          name: true,
+          email: true,
+          username: true,
+          imageUrl: true,
+        },
+      },
+    },
+  });
+
+  return event;
+}
