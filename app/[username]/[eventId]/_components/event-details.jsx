ea026bf2bf5c833c7b69c489e47317c28458dee1
lobsang norbu <lnorbu151@gmail.com>
2024-11-07 03:02:02 -0500

    add small margin

--- app/[username]/[eventId]/_components/event-details.jsx
+++ app/[username]/[eventId]/_components/event-details.jsx
@@ -8,13 +8,15 @@ const EventDetails = ({ event }) => {
   //   console.log(event);
   return (
     <div className="border py-5 px-5 bg-white shadow-md rounded-xl">
-      <h1 className="text-2xl text-center mt-1 font-semibold">{event.title}</h1>
-      <div className="flex items-center mb-3">
-        <Avatar className="h-24 w-24 ">
+      <h1 className="text-2xl text-center mt-1 font-semibold mb-5">
+        {event.title}
+      </h1>
+      <div className="flex items-center mb-5">
+        <Avatar className="h-24 w-24">
           <AvatarImage src={user.imageUrl} alt={user.name} />
           <AvatarFallback>{user.name.charAt(0)}</AvatarFallback>
         </Avatar>
-        <div className="">
+        <div className="ml-2">
           <h2 className="text-xl">{user.name}</h2>
           <h2 className=" text-gray-500">@{user.username}</h2>
         </div>
