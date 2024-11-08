21fb31c6f0cd773101dd360eba653c5bd02d639c
lobsang norbu <lnorbu151@gmail.com>
2024-11-07 23:59:44 -0500

    add upcoming and past meeting

--- app/(main)/meetings/page.jsx
+++ app/(main)/meetings/page.jsx
@@ -1,7 +1,43 @@
-import React from "react";
+import React, { Suspense } from "react";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import MeetingList from "./_components/meeting-list";
+import { getUserMeetings } from "@/actions/meetings";
+
+export const metadata = {
+  title: "Your Meetings || mycalend",
+  description: "View and manage your upcoming and past meetings.",
+};
 
 const Meeting = () => {
-  return <div className="text-xs">Meeting</div>;
+  return (
+    <div className="border border-red-500">
+      <Tabs defaultValue="account" className="border border-blue-500">
+        <TabsList>
+          <TabsTrigger value="upcoming">Upcoming Meetings</TabsTrigger>
+          <TabsTrigger value="past">Past Meetings</TabsTrigger>
+        </TabsList>
+        <TabsContent value="upcoming">
+          <Suspense fallback={<div>Loading upcoming meetings...</div>}>
+            <UpcomingMeetings />
+          </Suspense>
+        </TabsContent>
+        <TabsContent value="past">
+          <Suspense fallback={<div>Loading past meetings...</div>}>
+            <PastMeetings />
+          </Suspense>
+        </TabsContent>
+      </Tabs>
+    </div>
+  );
 };
+
+async function UpcomingMeetings() {
+  const meetings = await getUserMeetings("upcoming");
+  return <MeetingList meetings={meetings} type="upcoming" />;
+}
+async function PastMeetings() {
+  const meetings = await getUserMeetings("past");
+  return <MeetingList meetings={meetings} type="past" />;
+}
 
 export default Meeting;
